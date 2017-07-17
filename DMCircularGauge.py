93bfb5a3fbec60a7530dfce491828a6117c3e53a
scalverson <alverson@slac.stanford.edu>
2017-07-17 16:34:14 -0700

    Cleaned up paintEvent method Added glass effects Moved QPainterPath and gradient object creation to __init__ Improved text label drawing implementation

--- DMCircularGauge.py
+++ DMCircularGauge.py
@@ -1,176 +1,178 @@
-from PyQt4.QtGui import QApplication, QWidget, QPainter, QPainterPath, QPen, QRadialGradient, QFont, QFontMetrics, QColor, QMatrix, QPolygonF
+from PyQt4.QtGui import (
+    QApplication, QWidget, QPainter, QPainterPath, QPen, QRadialGradient, QLinearGradient,
+    QFont, QFontMetrics, QColor, QPolygonF
+)
 from PyQt4.QtCore import Qt, QString, QPointF, QPoint, QRectF
+from numpy import linspace
+
 
 class DMCircularGauge(QWidget):
-    def __init__(self, channel='', lim_low_channel=30.0, lim_hi_channel=100.0):
-        super(DMCircularGauge,self).__init__()
+    percentage = 0.0
+
+    def __init__(self, channel='', lim_low_channel=0.0, lim_hi_channel=100.0):
+        super(DMCircularGauge, self).__init__()
 
         self._channel = channel
-        self._limits = [lim_low_channel,lim_hi_channel]
-        self.channel_value = 70
-        print self.percentage
+        self._limits = [lim_low_channel, lim_hi_channel]
+        self.channel_value = 49
 
-    def initUI(self):
-        pass
+        self.resize(400, 220)
+
+        self.dial_v_offset = self.height() * 0.1
+        if self.height() >= self.width() / 2 + self.dial_v_offset:
+            self.dial_height = (self.width() / 2.0)
+            self.dial_width = self.width()
+        else:
+            self.dial_height = self.height() - self.dial_v_offset
+            self.dial_width = self.dial_height * 2
+        self.dial_h_offset = (self.width() - self.dial_width) / 2.0
+
+        self.dial = QPainterPath(QPointF(0.0, 200))
+        self.dial.arcTo(0.0, 0.0, 400, 400, 180, -180)
+        self.dial.lineTo(400, 220.0)
+        self.dial.lineTo(0.0, 220.0)
+        self.dial.lineTo(0.0, 200)
+
+        self.dial_bg = QRadialGradient(QPointF(self.width()/2.0, self.height()-self.dial_v_offset), self.dial_height)
+        self.dial_bg.setColorAt(0, Qt.lightGray)
+        self.dial_bg.setColorAt(0.98, QColor(50, 50, 50, 255))
+        self.dial_bg.setColorAt(1, Qt.black)
+
+        needle_base_width = 28
+        self.needle_left = QPolygonF([QPoint(0, 0),
+                                      QPoint(0, -needle_base_width/2.0),
+                                      QPoint(self.dial_height*0.9, 0.0)])
+        self.needle_right = QPolygonF([QPoint(0, 0),
+                                      QPoint(0, needle_base_width / 2.0),
+                                      QPoint(self.dial_height * 0.9, 0.0)])
+
+        pin_diameter = 32
+        self.pin_rect = QRectF(-pin_diameter / 2.0, -pin_diameter / 2.0, pin_diameter, pin_diameter)
+        self.pin_bg = QRadialGradient(QPointF(0.0, -pin_diameter / 5.0), pin_diameter * 0.75)
+        self.pin_bg.setColorAt(0, Qt.lightGray)
+        self.pin_bg.setColorAt(1, Qt.black)
+
+    def _dial_rect(self):
+        return QRectF(0.0,
+                      self.height() - (self.dial_height + self.dial_v_offset),
+                      self.dial_width,
+                      self.dial_height * 2)
 
     def paintEvent(self, event):
-        height_offset = 50.0
-        if self.height() >= self.width()/2 + height_offset:
-            height = (self.width()/2.0)
-            width = self.width()
-        else:
-            height = self.height()-height_offset
-            width = height*2
-        width_offset = (self.width()-width)/2.0
-
         # Initialize QPainter properties
-        painter = QPainter(self)
+        painter = QPainter()
+        painter.begin(self)
         painter.setRenderHint(QPainter.Antialiasing)
+        if self.height() <= self.width() / 2.1:
+            v_scale = self.height()
+            h_scale = v_scale * 2.1
+        else:
+            h_scale = self.width()
+            v_scale = h_scale / 2.1
+        painter.save()
+        painter.scale(h_scale/400.0, v_scale/220.0)
 
-        # First main draw gauge arc
+        # First main draw gauge background
         pen = QPen(painter.pen())
-        pen.setWidth(3)
+        pen.setWidth(1)
         pen.setColor(Qt.black)
         painter.setPen(pen)
-        dial_bg = QRadialGradient(QPointF(self.width()/2.0, self.height()-height_offset), height)
-        dial_bg.setColorAt(0, Qt.lightGray)
-        dial_bg.setColorAt(1, Qt.black)
-        painter.setBrush(dial_bg)
-        arc_path = QPainterPath(QPointF(width_offset, self.height()-height_offset))
-        arc_path.arcTo(QRectF(width_offset, self.height()-(height+height_offset), width, height*2), 180, -180)
-        painter.drawPath(arc_path)
+        painter.setBrush(self.dial_bg)
+        painter.drawPath(self.dial)
+        painter.restore()
 
-        # Add bottom block for label and overflow
-        bottom_path = QPainterPath(QPointF(width_offset, self.height()-height_offset-1))
-        bottom_path.lineTo(QPointF(width_offset, self.height()))
-        bottom_path.lineTo(QPointF(width+width_offset, self.height()))
-        bottom_path.lineTo(QPointF(width + width_offset, self.height()-height_offset-1))
-        painter.drawPath(bottom_path)
-
-        # Make the painter ready to draw scale labels
-        #drawWidth = self.width() / 100
-        font = QFont(painter.font())
-        font.setPixelSize(max(width/50, 10))
-        font_metric = QFontMetrics(font)
-        painter.setFont(font)
-        pen.setColor(Qt.white)
+        # Next add division markers
+        painter.save()
+        painter.scale(h_scale / 400.0, v_scale / 220.0)
+        painter.translate(self.dial_width / 2, self.dial_height * 0.98)
+        pen.setColor(Qt.cyan)
         painter.setPen(pen)
-
-        percentIncrease = 1 / 8.0
-        #print scale_text.size(), percentIncrease
-
-        inc = (self.lim_hi-self.lim_low)/8.0
-        scale = self.lim_low
-        percent = 0.0
-        for i in range(0,8,1):
-            #print percent
-            point = QPointF(arc_path.pointAtPercent(percent))
-            angle = arc_path.angleAtPercent(percent) # Clockwise is negative
-
-            painter.save()
-            # Move the virtual origin to the point on the curve
-            painter.translate(point)
-            # Rotate to match the angle of the curve
-            # Clockwise is positive so we negate the angle from above
-            painter.rotate(-angle)
-            # Draw a line width above the origin to move the text above the line
-            # and let Qt do the transformations
-            #print scale
-            painter.drawText(QPoint(0, pen.width() + 10), QString("|"))
-            painter.restore()
-
-            point = QPointF(arc_path.pointAtPercent(max(percent-0.01,0.0)))
-            angle = arc_path.angleAtPercent(max(percent-0.01,0.0))
-            painter.save()
-            scale_str = QString(str(scale))
-            painter.translate(point)
-            painter.rotate(-angle)
-            painter.drawText(0, pen.width() + 30, font_metric.width(scale_str), font_metric.height(),
-                             Qt.AlignHCenter, scale_str)
-            painter.restore()
-            scale += inc
-            percent += percentIncrease
-        point = QPointF(arc_path.pointAtPercent(percent))
-        angle = arc_path.angleAtPercent(percent)  # Clockwise is negative
-        painter.save()
-        painter.translate(point)
-        painter.rotate(-angle)
-        painter.drawText(QPoint(0, pen.width() + 10), QString("|"))
-        painter.restore()
-        point = QPointF(arc_path.pointAtPercent(max(percent - 0.01, 0.0)))
-        angle = arc_path.angleAtPercent(max(percent - 0.01, 0.0))
-        painter.save()
-        scale_str = QString(str(scale))
-        painter.translate(point)
-        painter.rotate(-angle)
-        painter.drawText(0, pen.width() + 30, font_metric.width(scale_str), font_metric.height(),
-                         Qt.AlignHCenter, scale_str)
+        for i in range(0, 31):
+            if (i % 5) != 0:
+                painter.drawLine(-self.dial_width / 2.1, 0.0, -self.dial_width / 2.2, 0.0)
+            else:
+                painter.drawLine(-self.dial_width / 2.1, 0.0, -self.dial_width / 2.3, 0.0)
+            painter.rotate(6.0)
         painter.restore()
 
-        needle_base_width = 14
-        #needle_length = height*0.9
-        needle_tip = QPointF(arc_path.pointAtPercent(self.percentage))
-        needle_base_center = QPointF(self.width() / 2, self.height() - height_offset)
-        needle_angle = arc_path.angleAtPercent(self.percentage)
-        #left_edge_angle = needle_angle-90
-        #right_edge_angle = needle_anlge+90
+        # Layout division text labels
+        painter.save()
+        painter.scale(h_scale / 400.0, v_scale / 220.0)
+        painter.translate(self.dial_width / 2, self.dial_height * 0.98)
+        pen.setColor(Qt.cyan)
+        painter.setPen(pen)
+        font = QFont(painter.font())
+        font_metric = QFontMetrics(font)
+        labels = linspace(self.lim_low, self.lim_hi, 7)
+        painter.rotate(-90)
+        for i in range(0, 7):
+            label = QString().setNum(labels[i], 'f', 2)
+            painter.drawText(QPointF(0.0 - font_metric.width(label) / 2.0, -self.dial_height * 0.80), label)
+            painter.rotate(30)
+        painter.restore()
 
+        # Draw needle at appropriate angle for data
+        painter.save()
+        painter.scale(h_scale / 400.0, v_scale / 220.0)
+        painter.translate(self.dial_width / 2, self.dial_height * 0.98)
+        painter.rotate(-180 * (1.0 - self.percentage))
+
+        if self.percentage >= 0.5:
+            needle_left_color = Qt.cyan  # QColor(230,230,230,255)
+            needle_right_color = Qt.darkCyan  # QColor(80,80,80,255)
+        else:
+            needle_left_color = Qt.darkCyan  # QColor(80,80,80,255)
+            needle_right_color = Qt.cyan  # QColor(230,230,230,255)
 
         # Draw Highlight side of needle
-        #pen = QPen(painter.pen())
         pen.setWidth(1)
-        pen.setColor(Qt.cyan) # QColor(230,230,230,255))
+        pen.setColor(needle_left_color)
         painter.setPen(pen)
-        painter.setBrush(Qt.cyan) #QColor(230,230,230,255))
-        #painter.save()
-        #painter.rotate(needle_angle)
-        needle_left = QPolygonF([QPointF(self.width() / 2 - needle_base_width/2, self.height() - height_offset),
-                                 needle_base_center,
-                                 needle_tip])
-        #painter.drawPolygon(QPointF(self.width() / 2 - needle_base_width/2, self.height() - height_offset),
-        #                    needle_base_center,
-        #                    needle_tip)
-        #transform = QMatrix()
-        #transform.rotate(10)
-        #needle_left = transform.map(needle_left)
-        painter.drawPolygon(needle_left)
-        #painter.restore()
+        painter.setBrush(needle_left_color)
+        painter.drawPolygon(self.needle_left)
 
         # Draw shadow side of needle
-        #pen = QPen(painter.pen())
-        #pen.setWidth(1)
-        pen.setColor(Qt.darkCyan) # QColor(80,80,80,255))
+        pen.setColor(needle_right_color)
         painter.setPen(pen)
-        painter.setBrush(Qt.darkCyan) # QColor(80,80,80,255))
-        needle_right = QPolygonF([QPointF(self.width() / 2 + needle_base_width / 2, self.height() - height_offset),
-                                 needle_base_center,
-                                 needle_tip])
-        #painter.save()
-        #painter.rotate(needle_angle)
-        #painter.drawPolygon(QPointF(self.width() / 2 + needle_base_width/2, self.height() - height_offset),
-        #                    needle_base_center,
-        #                    needle_tip)
-        #transform = QMatrix()
-        #transform.rotate(needle_angle)
-        #needle_right = transform.map(needle_right)
-        painter.drawPolygon(needle_right)
-        #painter.restore()
+        painter.setBrush(needle_right_color)
+        painter.drawPolygon(self.needle_right)
+        painter.restore()
 
         # Draw needle axel pin
-        pin_diameter = 20
+        painter.save()
         pen = QPen(painter.pen())
         pen.setWidth(1)
         pen.setColor(Qt.black)
         painter.setPen(pen)
-        pin_bg = QRadialGradient(QPointF(self.width() / 2.0 - 4, self.height() - height_offset - 5), 15)
-        pin_bg.setColorAt(0, Qt.lightGray)
-        pin_bg.setColorAt(1, Qt.black)
-        painter.setBrush(pin_bg)
-        painter.drawEllipse((self.width() / 2) - (pin_diameter/2),
-                            self.height() - height_offset - (pin_diameter/2),
-                            pin_diameter,
-                            pin_diameter)
+        painter.setBrush(self.pin_bg)
+        painter.scale(h_scale / 400.0, v_scale / 220.0)
+        painter.translate(self.dial_width / 2, self.dial_height * 0.98)
+        painter.drawEllipse(self.pin_rect)
+        painter.restore()
 
+        # Draw glass reflection and shadow effects
+        painter.save()
+        painter.scale(h_scale / 400.0, v_scale / 220.0)
+        painter.setClipPath(self.dial)
+        painter.translate(self.dial_width / 2.0, self.dial_height / 2.0)
+        painter.setPen(Qt.NoPen)
+        painter.setBrush(QColor(0, 0, 0, 20))
+        painter.drawEllipse(-self.dial_width / 2,
+                            -self.dial_height / 2,
+                            self.dial_width,
+                            self.dial_height * 1.1)
+        gloss = QLinearGradient(QPointF(0.0, -self.dial_height / 2), QPointF(0.0, 0.0))
+        gloss.setColorAt(0.0, QColor(255, 255, 255, 120))
+        gloss.setColorAt(0.95, QColor(255, 255, 255, 0))
+        painter.setBrush(gloss)
+        painter.drawEllipse(-self.dial_width / 5,
+                            -self.dial_height / 2,
+                            self.dial_width / 2.5,
+                            self.dial_height / 2)
+
+        painter.restore()
+
+        painter.end()
 
     @property
     def lim_hi(self):
