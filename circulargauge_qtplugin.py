fc99fe5183ca946e2f7a7b916fed9620a9128f2b
scalverson <alverson@slac.stanford.edu>
2017-08-01 10:31:28 -0700

    Minor syntax fix

--- circulargauge_qtplugin.py
+++ circulargauge_qtplugin.py
@@ -1,4 +1,4 @@
-from .qtplugin_base import qtplugin_factory
+from qtplugin_base import qtplugin_factory
 from DMCircularGauge import DMCircularGauge
 
 DMCircularGaugePlugin = qtplugin_factory(DMCircularGauge)
